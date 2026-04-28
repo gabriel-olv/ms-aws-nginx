44367868a5b9c725efd382b421c2f6f3d0bf1073
Gabriel de Oliveira <br.gabrieldeoliveira@gmail.com>
2026-04-27 21:27:21 -0300

    Project created

---
+++ src/main/java/br/com/gbrlo/learning/user/UserMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbrlo.learning.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+
+}
